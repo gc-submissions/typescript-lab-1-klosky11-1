c7ec45139f8f58b57f539b879e2d040132c1f169
Teri Klosky <t.klosky@yahoo.com>
2022-01-12 21:12:11 -0500

    return change

--- src/mountains.ts
+++ src/mountains.ts
@@ -21,17 +21,20 @@ const mountains: Mountain[] = [
 console.log(mountains);
 
 export function findNameOfTallestMountain(array: Mountain[]): string {
-  let tallestMountainHeight: Mountain = array[0];
-  if (array.length !== 0) {
-    for (let mountain of array) {
-      if (mountain.height > tallestMountainHeight.height) {
-        tallestMountainHeight = mountain;
-      }
-    }
-    return tallestMountainHeight.name;
+  if (array.length === 0) {
+    return "";
   }
-  return "";
+  let tallestMountainHeight: Mountain = array[0];
+  //   if (array.length !== 0) {
+  for (let mountain of array) {
+    if (mountain.height > tallestMountainHeight.height) {
+      tallestMountainHeight = mountain;
+    }
+  }
+  return tallestMountainHeight.name;
 }
+//   return "";
+// }
 
 console.log(findNameOfTallestMountain(mountains));
 console.log(findNameOfTallestMountain([]));
